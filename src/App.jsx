0ed3b867f7ede86c52b48b7e739967c1c17dab89
philmist <philmist.j@gmail.com>
2022-09-14 11:21:53 +0900

    JSONをデザインなしで整形して表示させる部分を書いた

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,59 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import masterData from '../json-data/pasture.json'
+
+function MasterRow(props) {
+  const name = props.name;
+  const product = props.product;
+  return (
+    <li>
+      <dl>
+      <dt>{name}</dt>
+        <dd>
+          <dl>
+            <div>
+              <dt>所要時間</dt>
+              <dd>{product["hour"]}</dd>
+            </div>
+            <div>
+              <dt>出荷単位</dt>
+              <dd>{product["unit"]}</dd>
+            </div>
+            <div>
+              <dt>基礎価値</dt>
+              <dd>{product["value"]}</dd>
+            </div>
+            <div>
+              <dt>特性</dt>
+              <dd><ul>{product["treat"].map(e => (
+                <li key={e}>
+                  {e}
+                </li>
+              ))}</ul></dd>
+            </div>
+            <div>
+              <dt>必要素材</dt>
+              <dd><ul>{Object.entries(product["material"]).map(e => (
+                <li key={e[0]}>{e[0]}: {e[1]}</li>
+              ))}</ul></dd>
+            </div>
+          </dl>
+        </dd>
+      </dl>
+    </li>
+  );
+}
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ul>
+        {Object.entries(masterData["master"]).map(e => (
+          <MasterRow key={e[0]} name={e[0]} product={e[1]} />
+        ))}
+      </ul>
     </div>
   )
 }
